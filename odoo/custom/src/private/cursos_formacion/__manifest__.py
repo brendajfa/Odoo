dffb687c6c179c387bd6b1d05236f8cb21f6a655
brendajfa <breferala99@gmail.com>
2023-05-30 16:33:43 +0200

    update

--- odoo/custom/src/private/cursos_formacion/__manifest__.py
+++ odoo/custom/src/private/cursos_formacion/__manifest__.py
@@ -3,25 +3,26 @@
 {
     'name': 'Cursos de Formación',
     'version': '13.0.1.0.0',
-    'description': '',
+    # 'description': '',
     'summary': 'registra los cursos de formación',
     'author': 'QubiQ',
-    'website': 'https://www.qubiq.es',
+    'website': 'www.qubiq.es',
     'license': 'AGPL-3',#'LGPL-3',
     'category': 'Base',
     'depends': [
         ''
     ],
     'data': [
-        'views/cursos_formacion.xml'
+        'views/cursos_formacion.xml',
+        'security/ir.model.access.csv'
     ],
-    'demo': [
-        ''
-    ],
-    'auto_install': False,
+    # 'demo': [
+    #     ''
+    # ],
+    # 'auto_install': False,
     'application': False,
     'installable': True,
-    'assets': {
+    # 'assets': {
         
-    }
+    # }
 }
--- odoo/custom/src/private/cursos_formacion/models/cursos_formacion.py
+++ odoo/custom/src/private/cursos_formacion/models/cursos_formacion.py
@@ -1,11 +1,15 @@
 from odoo  import fields, models
 
 class CursosFormacion(models.Model):
-    _name = "cursos.formacion"
-    _description = 'Modelo para registrar cursos'
+    _name = 'cursos.formacion'
+    _description = 'Modelo para reg'
 
     name = fields.Char()
     price = fields.Float()
     date = fields.Date(string="Date Purchase")
-    course_type = fields.Selection(selection=[('normal', 'Normal'), ('online', 'Online')], string='Type')
+    course_type = fields.Selection(selection=[
+        ('normal', 'Normal'),
+        ('online', 'Online')
+    ], string='Type')
+    
     adress = fields.Char()
